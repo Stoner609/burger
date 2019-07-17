57502e1795802dfd0c69f76c039817ed9db52ec8
stoner609 <stoner7969@gmail.com>
2019-07-17 19:21:47 +0800

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BurgerControls/BurgerControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+}
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+    purchasable: false,
+  }
+
+  updatePurchaseState () {
+    let sum = Object.keys(this.state.ingredients)
+        .map(igKey => this.state.ingredients[igKey])
+        .reduce((total, el) => {
+          return total + el
+        }, 0);
+
+    this.setState((prev) => ({
+      purchasable: sum > 0
+    }));
+  }
+
+  addIngredientHandler = (type) => {
+    const priceAddition = INGREDIENT_PRICES[type];
+    
+    this.setState((prev) => ({
+        ingredients: {
+          ...prev.ingredients,
+          [type]: prev.ingredients[type] + 1
+        },
+        totalPrice: prev.totalPrice + priceAddition
+    }), this.updatePurchaseState);
+  }
+
+  removeIngredientHandler = (type) => {
+    if (this.state.ingredients[type] <= 0) return;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    this.setState((prev) => ({
+        ingredients: {
+          ...prev.ingredients,
+          [type]: prev.ingredients[type] - 1
+        },
+        totalPrice: prev.totalPrice - priceAddition
+    }), this.updatePurchaseState);
+  }
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          purchasable={this.state.purchasable}
+          price={this.state.totalPrice}
+        />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
