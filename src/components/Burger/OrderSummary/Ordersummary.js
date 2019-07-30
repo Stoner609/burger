507c5f1067624d963a70494234bb52d221dfb79c
許浩哲 <stoner.hsu@bigbear.me>
2019-07-30 10:17:54 +0800

    improving Performance

--- src/components/Burger/OrderSummary/Ordersummary.js
+++ src/components/Burger/OrderSummary/Ordersummary.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
       .map(igKey => {
         return (
           <li key={igKey}>
             <span style={{textTransform: 'capitalize'}}>{igKey}</span>: 
-            {props.ingredients[igKey]}
+            {this.props.ingredients[igKey]}
           </li>);
-      })
+      });
 
-  return (
-    <Aux>
-      <h3>你的訂單</h3>
-      <p>一種美味的漢堡包，含了以下成分</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>總共價錢：{props.price}</strong></p>
-      <p>繼續結帳?</p>
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>取消</Button>
-      <Button btnType='Success' clicked={props.purchaseContunued}>繼續</Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>你的訂單</h3>
+        <p>一種美味的漢堡包，含了以下成分</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>總共價錢：{this.props.price}</strong></p>
+        <p>繼續結帳?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>取消</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContunued}>繼續</Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
