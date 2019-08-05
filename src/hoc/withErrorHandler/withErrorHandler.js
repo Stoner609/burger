d8e29d3b294b9eb83c7b9bd21797cc9d3efde624
許浩哲 <stoner.hsu@bigbear.me>
2019-08-05 15:06:52 +0800

    add componentWillUnmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,18 +10,20 @@ const withErrorHandler = (WappedComponent, axios) => {
     }
     
     componentDidMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       });
 
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({error: error});
       });
     }
 
-    componentDidCatch(error, info) {
-      console.log(error);
+    componentWillUnmount() {
+      console.log('will Unmount', this.reqInterceptor, this.resInterceptor, );
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmHandler = () => {
