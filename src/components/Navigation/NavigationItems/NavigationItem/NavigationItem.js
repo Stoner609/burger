6ff3921e946f30a539a17506421d91bfeda4d4e3
許浩哲 <stoner.hsu@bigbear.me>
2019-08-15 18:12:38 +0800

    add NavLink

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.scss';
 
-const navigetionItem = (props) => (
-  <li className={classes.NavigationItem}>
-    <a 
-      href={props.link} 
-      className={props.active ? classes.active : null}
-    >
-      {props.children}
-    </a>
-  </li>
-);
+const navigetionItem = (props) => {
+  return (
+    <li className={classes.NavigationItem}>
+      <NavLink 
+        to={props.link}
+        exact={props.exact}
+        activeClassName={classes.active}>{props.children}</NavLink>
+    </li>
+  );
+};
 
 export default navigetionItem;
